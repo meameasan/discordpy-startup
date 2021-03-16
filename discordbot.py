b8b2b2fb6444fd24650bc486a6ed8991fcf5f779
meameasan <80627925+meameasan@users.noreply.github.com>
2021-03-17 05:42:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,11 +11,24 @@ async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
+    
+@bot.event
+async def on_message(message)
+if message.author.bot:
+    return
+if message.content =="/neko":
+    await message.channel.send("にゃーん")
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+@client.event
+async def on_message_delete(message):
+    channel = client.get_channel(DEBUG_CHANNEL_ID)
+    await channel.send(f"{message.author.name}さんのメッセージが削除されました:\n```\n{message.content}\n```")
+
 
 
 bot.run(token)
